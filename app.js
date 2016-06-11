c756872523d63f43ac3e657dacfa4475f62ac731
Mykola Senyk <msenyk@corevalue.net>
2016-06-12 00:32:58 +0300

    Added body load

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const redis = require('redis');
 let client = redis.createClient();
 
 const MAX_LIST_SIZE = 5;
+const MAX_BODY_SIZE = 1 * 1024;
 const LIST_TTL = 10;
 const PORT = '8000';
 const HOSTNAME = 'localhost';
@@ -11,8 +12,8 @@ const HOSTNAME = 'localhost';
 const cKey = 'a1b2c3d4e5f6g7h8';
 let i = 0;
 
-const saveRequest = (cKey, cb) => {
-	client.lpush(cKey, i++, (err, listSize) => {
+const saveRequest = (cKey, value, cb) => {
+	client.lpush(cKey, (i++) + ' ' + JSON.stringify(value, null, '  '), (err, listSize) => {
     	console.log('size: ' + listSize);
     	if ( listSize > MAX_LIST_SIZE ) {
     		client.rpop(cKey);
@@ -30,15 +31,46 @@ const saveRequest = (cKey, cb) => {
 
 client.on('connect', () => {
     console.log('connected');
-    saveRequest(cKey);
+    //saveRequest(cKey);
 });
 
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	saveRequest(cKey, (l) => {
-		res.end(l + '\n');
+	res.setHeader('Content-Type', 'text/plain;charset=utf8');
+	const today = new Date();
+	const value = {
+		created: today.toUTCString(),
+		ip: req.connection.remoteAddress,
+		method: req.method,
+		url: req.url,
+		headers: req.headers
+	};
+	const body = [];
+	let bodySize = 0;
+	//req.setEncoding('utf8');
+	req.on('data', (chunk) => {
+		console.log('chunk: ' + chunk);
+		if ( bodySize < MAX_BODY_SIZE ) {
+			if ( bodySize + chunk.length > MAX_BODY_SIZE ) {
+				console.log('Skip end of body');
+				const trimmedLength = MAX_BODY_SIZE - bodySize;
+				console.log('trimmed length: ' + trimmedLength);
+				chunk = chunk.slice(0, trimmedLength);
+
+			}
+			body.push(chunk);
+			bodySize += chunk.length;
+		}
 	});
+	req.on('end', () => {
+		value.body = Buffer.concat(body).toString();
+		console.log('end of body\n' + value.body);
+
+		saveRequest(cKey, value, (l) => {
+			res.end(l.join('\n') + '\n');
+		});
+	});
+	
 });
 
 server.listen(PORT, HOSTNAME, () => {
