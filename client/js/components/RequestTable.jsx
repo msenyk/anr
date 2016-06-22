89315df0056ee58bc77e15ecb37df4754fc87626
Mykola Senyk <msenyk@corevalue.net>
2016-06-23 00:42:17 +0300

    Sample data table rendering

--- client/js/components/RequestTable.jsx
+++ client/js/components/RequestTable.jsx
@@ -1,19 +1,58 @@
 import React from 'react'
+import { Button, Table } from 'react-bootstrap';
 
 export default React.createClass({
+	buildRows() {
+		return this.props.rows.map((row, index) => (<RequestRow key={index} row={row}></RequestRow>));
+	},
 	render() {
 		return (
-			<div>
-				Table
-			</div>
+			<Table responsive hover>
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Created</th>
+						<th>IP</th>
+						<th>Method</th>
+						<th>URL</th>
+						<th>Headers</th>
+						<th>Body</th>
+					</tr>
+				</thead>
+				<tbody>
+					{ this.buildRows() }
+				</tbody>
+			</Table>
 		);
 	}
 })
 
 const RequestRow = React.createClass({
+	getInitialState() {
+		return {
+			isShowHeaders: false,
+			isShowBody: false
+		}
+	},
+	createdDate() {
+		return new Date(this.props.row.created).toString();
+	},
+	showHideHeaders() {
+		this.setState({
+			isShowHeaders: !this.state.isShowHeaders
+		});
+	},
 	render() {
 		return (
-			<div>ROW</div>
+			<tr>
+				<td></td>
+				<td>{ this.createdDate() }</td>
+				<td>{ this.props.row.ip }</td>
+				<td>{ this.props.row.method }</td>
+				<td>{ this.props.row.url}</td>
+				<td><Button onClick={ this.showHideHeaders }>{ this.state.isShowHeaders ? 'Hide' : 'Show' }</Button></td>
+				<td> show body </td>
+			</tr>
 		)
 	}
 })
--- client/js/views/Home.jsx
+++ client/js/views/Home.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 import RequestTable from '../components/RequestTable.jsx'
 
+const requestLogRecords = [
+	{
+		created: new Date().valueOf(),
+		ip: '127.0.0.1',
+		method: 'POST',
+		url: 'query?id=2',
+		headers: {'content-type': 'text/plain'},
+		body: 'Simple body example'
+	},
+	{
+		created: 1233443322899,
+		ip: '127.0.0.2',
+		method: 'GET',
+		url: 'query?id=1&a=b',
+		headers: {'content-type': 'text/html'}
+	}
+];
+
 export default React.createClass({
 	render() {
 		return (
@@ -8,7 +26,7 @@ export default React.createClass({
 				<div className="container">
 					<h1>Recent requests</h1>
 					<p>{this.props.clientId}</p>
-					<RequestTable />
+					<RequestTable rows={requestLogRecords} />
 				</div>
 			</div>
 		);
